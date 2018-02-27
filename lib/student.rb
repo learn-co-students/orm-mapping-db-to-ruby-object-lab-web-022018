d5d940a27fa59cbeb531dc7808cddf2e95bce82d
Ray Blasko <mray6288@gmail.com>
2018-02-27 15:42:45 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,37 @@
+require 'pry'
+
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    student = Student.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
+
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      select * from students
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+      select * from students where name = ? limit 1
+    SQL
+
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
   
   def save
@@ -40,4 +59,53 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      select count(grade) from students
+      where grade = ?
+      SQL
+
+    DB[:conn].execute(sql, '9')
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      select count(grade) from students
+      where grade < ?
+      SQL
+
+    DB[:conn].execute(sql, '12')
+  end
+
+  def self.first_X_students_in_grade_10(num)
+    sql = <<-SQL
+      select * from students
+      where grade = ?
+      limit ?
+      SQL
+
+    DB[:conn].execute(sql, '10', num)
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      select * from students
+      where grade = ?
+      limit 1
+      SQL
+
+    new_from_db(DB[:conn].execute(sql, '10')[0])
+
+  end
+
+  def self.all_students_in_grade_X(num)
+    sql = <<-SQL
+      select * from students
+      where grade = ?
+      SQL
+
+    DB[:conn].execute(sql, num)
+  end
+
 end
